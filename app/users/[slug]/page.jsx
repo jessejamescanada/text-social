3e2f95414bee272a75161e30ac98e49f7aeb582f
Jesse Giammattolo <giammattolo@hotmail.com>
2023-04-19 19:26:58 -0400

    added check for image before render on /users profile

--- app/users/[slug]/page.jsx
+++ app/users/[slug]/page.jsx
@@ -20,13 +20,15 @@ const UsersPage = ({ params: { slug } }) => {
   return (
     <div className='w-full flex flex-col items-center justify-center'>
       <div className='flex flex-col sm:flex-row w-full sm:w-1/2 justify-center items-center my-4'>
-        <Image
-          src={data.image}
-          height={80}
-          width={80}
-          alt='image'
-          className='rounded-md'
-        />
+        {data.image && (
+          <Image
+            src={data.image}
+            height={80}
+            width={80}
+            alt='image'
+            className='rounded-md'
+          />
+        )}
         <div className='flex flex-col ml-5'>
           <div className='flex flex-row gap-4 items-center mb-2'>
             <h3>{data.name}</h3>
