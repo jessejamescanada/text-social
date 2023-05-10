801ce6897a3c0fd3ef187c4fff4e50c3bdd47f0b
Jesse Giammattolo <giammattolo@hotmail.com>
2023-05-10 14:31:29 -0400

    fixed mobile height messenger modal

--- components/MessengerModal.jsx
+++ components/MessengerModal.jsx
@@ -2,7 +2,7 @@ import FriendsForMessenger from './FriendsForMessenger'
 
 const MessengerModal = ({ session, friendsName, setFriendsName }) => {
   return (
-    <div className=' absolute md:flex  md:ml-10 top-[150px] mx-auto md:mx-0 mt-2  w-[90%] md:w-[250px] h-[85vh] md:h-[400px]  inset-0 overflow-y-hidden rounded-lg z-[2000] '>
+    <div className=' absolute md:flex  md:ml-10 top-[150px] mx-auto md:mx-0 mt-2  w-[90%] md:w-[250px] h-[75vh] md:h-[400px]  inset-0 overflow-y-hidden rounded-lg z-[2000] '>
       <div className='flex items-start w-full  h-full justify-start'>
         <div className='relative bg-white w-full h-full rounded-lg rounded-t-lg '>
           <div className=' h-full sm:h-[400px] z-[100]'>
