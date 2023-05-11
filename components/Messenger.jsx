aa612261c91c598e1142e6722880744f34368e61
Jesse Giammattolo <giammattolo@hotmail.com>
2023-05-11 11:12:55 -0400

    fixed Messenger Modal Title when user clicks Messages btn it resets properly

--- components/Messenger.jsx
+++ components/Messenger.jsx
@@ -8,10 +8,15 @@ const Messenger = ({ session }) => {
   const [friendsBack, setFriendsBack] = useState(true)
   const [friendsName, setFriendsName] = useState('')
 
+  const setMessengerName = () => {
+    setShowModal((prev) => !prev)
+    setFriendsName('Your Friends')
+  }
+
   return (
     <div className=' pl-1 sm:pl-8'>
       <button
-        onClick={() => setShowModal((prev) => !prev)}
+        onClick={setMessengerName}
         className='bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-lg disabled:opacity-50 disabled:cursor-not-allowed'
         disabled={!session}
       >
